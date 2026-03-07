d8d2b0feb5540fa70fd8f7e2f7c600edbd14aec5
Берсенев Сергей Сергеевич <sbersenev@yandex.ru>
2026-03-07 12:58:15 +0300

    Добавил faker и вынес линки

--- src/test/java/UserCreateTests.java
+++ src/test/java/UserCreateTests.java
@@ -3,28 +3,32 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import util.Requests;
+import net.datafaker.Faker;
 
 import static config.RestAssuredConfig.configRestAssured;
+import static constants.Urls.APP_URL;
+import static constants.Urls.REGISTER_ENDPOINT;
 
 public class UserCreateTests {
     private Response response;
     private Requests requests;
     private UserBodyDto userJsonBody = new UserBodyDto();
+    private final Faker faker = new Faker();
 
     @BeforeEach
     void setUp() {
-        requests = new Requests("https://stellarburgers.education-services.ru");
+        requests = new Requests(APP_URL);
         configRestAssured();
         userJsonBody = userJsonBody.toBuilder()
-                .email("RandomUser9993@mail.ru")
-                .password("1234")
-                .name("Вася")
+                .email(faker.internet().emailAddress())
+                .password(faker.lorem().characters(10))
+                .name(faker.name().firstName())
                 .build();
     }
 
     @Test
     void userCreateTest() {
-        response = requests.post("api/auth/register", userJsonBody);
+        response = requests.post(REGISTER_ENDPOINT, userJsonBody);
         response.then().assertThat().statusCode(200);
     }
 }
---
+++ src/test/java/constants/Urls.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Urls {
+    public static String APP_URL = "https://stellarburgers.education-services.ru";
+
+    public static final String REGISTER_ENDPOINT = "/api/auth/register";
+    public static final String LOGIN_ENDPOINT = "/api/auth/login";
+    public static final String USER_ENDPOINT = "/api/auth/user";
+    public static final String ORDERS_ENDPOINT = "/api/orders";
+}
