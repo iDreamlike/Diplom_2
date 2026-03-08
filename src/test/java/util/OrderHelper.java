4b4df2658bd63a7dd5c25d81db38ec26ebbe74ba
Берсенев Сергей Сергеевич <sbersenev@yandex.ru>
2026-03-08 23:10:08 +0300

    Добавил шаги

--- src/test/java/util/OrderHelper.java
+++ src/test/java/util/OrderHelper.java
@@ -1,5 +1,6 @@
 package util;
 
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 import java.util.List;
@@ -10,34 +11,43 @@ import static util.HttpRequests.get;
 
 
 public class OrderHelper {
+
+    @Step("Создание заказа с ингредиентами и авторизацией")
     public static Response createOrderWithIngredientsWithAuth(List<String> ingredients, String accessToken) {
         return post(ORDERS_ENDPOINT, ingredients, accessToken);
     }
 
+    @Step("Создание заказа с ингредиентами и без авторизации")
     public static Response createOrderWithIngredientsWithoutAuth(List<String> ingredients) {
         return post(ORDERS_ENDPOINT, ingredients);
     }
 
+    @Step("Создание заказа без ингредиентов с авторизацией")
     public static Response createOrderWithoutIngredientsWithAuth(List<String> ingredients, String accessToken) {
         return post(ORDERS_ENDPOINT, ingredients, accessToken);
     }
 
+    @Step("Создание заказа без ингредиентов и без авторизации")
     public static Response createOrderWithoutIngredientsWithoutAuth(List<String> ingredients) {
         return post(ORDERS_ENDPOINT, ingredients);
     }
 
+    @Step("Создание заказа с некорректными ингредиентами и с авторизацией")
     public static Response createOrderWithIncorrectIngredientsWithAuth(List<String> ingredients, String accessToken) {
         return post(ORDERS_ENDPOINT, ingredients, accessToken);
     }
 
+    @Step("Создание заказа с некорректными ингредиентами и без авторизации")
     public static Response createOrderWithIncorrectIngredientsWithoutAuth(List<String> ingredients) {
         return post(ORDERS_ENDPOINT, ingredients);
     }
 
+    @Step("Получение заказов с авторизацией")
     public static Response getOrdersWithAuthUser(String accessToken) {
         return get(ORDERS_ENDPOINT, accessToken);
     }
 
+    @Step("Получение заказов без авторизации")
     public static Response getOrdersWithoutAuthUser() {
         return get(ORDERS_ENDPOINT);
     }
--- src/test/java/util/UserHelper.java
+++ src/test/java/util/UserHelper.java
@@ -2,6 +2,7 @@ package util;
 
 import dto.LoginBodyDto;
 import dto.UserBodyDto;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 import static constants.Urls.*;
@@ -9,26 +10,32 @@ import static util.HttpRequests.*;
 
 public class UserHelper {
 
+    @Step("Получение токена")
     public static String getAccessToken(Response response) {
         return response.path("accessToken");
     }
 
+    @Step("Регистрация пользователя")
     public static Response registerUser(UserBodyDto user) {
         return post(REGISTER_ENDPOINT, user);
     }
 
+    @Step("Удаление пользователя")
     public static void deleteUser(String accessToken) {
         delete(USER_ENDPOINT, accessToken);
     }
 
+    @Step("Логин пользователя")
     public static Response loginUser(LoginBodyDto login) {
         return post(LOGIN_ENDPOINT, login);
     }
 
+    @Step("Обновление пользователя с авторизацией")
     public static Response updateUserWithAuthorization(UserBodyDto user, String accessToken) {
         return patch(USER_ENDPOINT, user, accessToken);
     }
 
+    @Step("Обновление пользователя без авторизации")
     public static Response updateUserWithoutAuthorization(UserBodyDto user) {
         return patch(USER_ENDPOINT, user);
     }
